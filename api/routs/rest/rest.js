9ef790ed3bcbbf583bef75ac1199d8fdeaee5f39
Tomas Mali <tommal@sanmarco.lan>
2020-08-02 18:15:59 +0200

    test ok

--- api/routs/rest/rest.js
+++ api/routs/rest/rest.js
@@ -14,13 +14,20 @@ const { isPrimitive } = require('util');
 // First route, get all users
 router.get('/', (req, res, next) => {
     const id = req.body.data;
+     fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            console.log(err);
+        } else {
+        obj = JSON.parse(data); //now it an object
+     
+        res.status(200)
+            .json(obj);
+    }});
+});
 
-
-
-
-
-
-
+// First route, get all users
+router.post('/', (req, res, next) => {
+    const id = req.body.data;
 
      fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
         if (err){
@@ -28,20 +35,41 @@ router.get('/', (req, res, next) => {
         } else {
         obj = JSON.parse(data); //now it an object
         obj.table.push(   {
-            timestamp: id.timestamp,
-            utente: id.utente,
-            password: id.password,
-            methodName: id.methodName,
-            parametri1:id.parametri1,
-            parametri2:id.parametri2
+
+            Funzione_chiamato: req.body.Funzione_chiamato,
+            Utente: req.body.Utente,
+            Password: req.body.Password,
+            Timestamp: req.body.Timestamp,
+            ARC: req.body.ARC,
+            Codice_Accisa: req.body.Codice_Accisa,
+            Id_Locale: req.body.Id_Locale,
+  
+            Fornitura: req.body.Fornitura,
+            Data: req.body.Data,
+            Punto_Di_Vista: req.body.Punto_Di_Vista,
+            Progressivo: req.body.Progressivo
+
+
+
+          
         }); //add some data
         json = JSON.stringify(obj); //convert it back to json
         fs.writeFile('myjsonfile.json', json, 'utf8', ()=>{
             res.status(200)
-            .json({ message:  id });
+            .json({ message:  {
+                    "Funzione_chiamato": req.body.Funzione_chiamato,
+                    "Utente": req.body.Utente,
+                    "Password": req.body.Password,
+                    "Timestamp": req.body.Timestamp,
+                    "ARC": req.body.ARC,
+                    "Codice_Accisa": req.body.Codice_Accisa,
+                    "Id_Locale": req.body.Id_Locale    
+            }
+        });
          });
     }});
 });
+
 
 
 
